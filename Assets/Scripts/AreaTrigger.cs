d16942be48e672f5e94267ac2b48764a73c35868
潘丁瑜 <2413110608@qq.com>
2023-12-30 18:53:50 +0800

    done my part

--- Assets/Scripts/AreaTrigger.cs
+++ Assets/Scripts/AreaTrigger.cs
@@ -7,14 +7,13 @@ public class AreaTrigger : MonoBehaviour
 {
     public Image imageToActivate;
     public Image imageNotActivate;
-
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (other.gameObject.CompareTag("Player")) // 假设进入触发器的是角色
+        if (collision.gameObject.CompareTag("Player")) // 假设进入触发器的是角色
         {
             Debug.Log("1");
             imageToActivate.gameObject.SetActive(true); // 激活Image
-            imageToActivate.gameObject.SetActive(false);
+            imageNotActivate.gameObject.SetActive(false);
         }
     }
 }
